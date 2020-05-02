f1334befc76d86d9f7e17c1d424eeb3bfce056ca
Kobie-Kirven <51420086+Kobie-Kirven@users.noreply.github.com>
2020-05-01 23:11:46 -0400

    Add files via upload

--- symrx_web/application.py
+++ symrx_web/application.py
@@ -9,3 +9,11 @@ def home():
 @app.route("/pictograms")
 def picto():
     return render_template("picto.html")
+
+@app.route("/about")
+def about():
+    return render_template("about_us.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
+  app.static_folder = 'static'
